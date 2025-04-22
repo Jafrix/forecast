4ee57f79ede79aa79e56ad985a164c20fd5fd09a
Andrey Veselov <lifeforglory@gmail.com>
2025-04-23 00:05:09 +0300

    kk2

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,45 @@ createRoot(document.getElementById('root')!).render(
     <App />
   // </StrictMode>,
 )
+
+
+// !=========================================================================
+// import React from "react";
+
+// const ToDoList = {
+//   today: [
+//     { id: 1, text: "Полить цветы" },
+//     { id: 2, text: "Помыть машину" },
+//     { id: 3, text: "Выкинуть мусор" },
+//   ],
+//   tomorrow: [],
+// };
+
+// const App = () => {
+//     return (
+//     <div>
+//       <h2>Сегодня:</h2>
+//       <div>
+//         <ul>
+//           {ToDoList.today.length &&
+//             ToDoList.today.map((item) => <li>{item.text}</li>)}
+//         </ul>
+//         <button>Удалить</button>
+//         <input />
+//         <button>Добавить</button>
+//       </div>
+//       <h2>Завтра:</h2>
+//       <div>
+//         <ul>
+//           {ToDoList.tomorrow.length &&
+//             ToDoList.tomorrow.map((item) => <li>{item.text}</li>)}
+//         </ul>
+//         <button>Удалить</button>
+//         <input />
+//         <button>Добавить</button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
